8f0a94770c89a6b9c5c2662be386712291ce7eb9
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-10-30 15:26:24 -0400

    Inquirer is set up correctly, Employee and manager JS written

---
+++ index.js
@@ -0,0 +1,155 @@
+const { TestWatcher } = require('@jest/core');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+const { inherits } = require('util');
+
+console.log('test to be sure its running')
+
+
+
+
+let EmployeeArray = []
+
+const questions = [
+    {
+        type:"Input",
+        name:"name",
+        message:"Add Manager name",
+    },
+    {
+        type:"Input",
+        name:"managerid",
+        message:"Insert Employee id",
+    },
+    {
+        type:"Input",
+        name:"manageremail",
+        message:"Enter Email",
+    },
+    {
+        type:"input",
+        name:"officenum",
+        message:"Office Number:",
+    },
+    {
+        type:"list",
+        name:"additional",
+        message:"Add another employee?",
+        choices:['Engineer','Intern', 'Finished']
+    },
+]
+
+
+//if Engineer
+const EngQuestions =[
+    {
+        type:"input",
+        name:"EngineerName",
+        message:"Add Engineers Name",
+    },
+    {
+        type:"Input",
+        name:"EngineerID",
+        message:"Add Employee ID",
+    
+    },
+    {
+        type:"input",
+        name:"EngineerEmail",
+        message:"Add Engineer Email",
+    },{
+        type:"Input",
+        name:"GithubUser",
+        message:"Add Github Username",
+    },
+    {
+        type:"list",
+        name:"additional",
+        message:"Add another employee?",
+        choices:['Engineer','Intern', 'Finished']
+    },
+
+]
+
+
+const internQuestions = [
+   
+
+    {
+        type:"input",
+        name:"InternName",
+        message:"Add Interns Name",
+    },
+    {
+        type:"Input",
+        name:"InternID",
+        message:"Add Intern ID",
+    
+    },
+    {
+        type:"input",
+        name:"InternEmail",
+        message:"Add Intern Email",
+    },
+    {
+        type:"input",
+        name:"InternSchool",
+        message:"Add Intern School",
+    },
+    {
+        type:"list",
+        name:"additional",
+        message:"Add another employee?",
+        choices:['Engineer','Intern', 'Finished']
+    },
+
+];
+
+
+
+
+function init(){
+    //
+    inquirer.prompt(questions)
+    .then((answers) =>{
+        console.log(answers);
+        EmployeeArray.push(answers)
+        Prompts(answers);
+    })
+}
+
+function Prompts(answers){
+    if(answers.additional === 'Engineer'){
+        EPrompts();
+    } else if (answers.additional === 'Intern'){
+        IPrompts();
+    } else {
+        console.log(EmployeeArray)
+        //function to create index.html
+    }
+}
+
+function EPrompts(){
+    inquirer.prompt(EngQuestions)
+    .then((answers) =>{
+        console.log(answers);
+        EmployeeArray.push(answers)
+        Prompts(answers);
+    })}
+
+    
+function IPrompts(){
+    inquirer.prompt(internQuestions)
+    .then((answers) =>{
+        console.log(answers);
+        EmployeeArray.push(answers)
+        Prompts(answers);
+    })
+}
+
+
+
+
+
+
+init();
